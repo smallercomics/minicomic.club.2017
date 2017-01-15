372210796f68176a9358295ff42154f7645a527e
Andrew Fulton <andrew@bundle.com.au>
2017-01-15 21:45:40 +1100

    Added generic page template and used it to make the new subscribers giveaway page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 const gulp = require('gulp');
 const connect = require('gulp-connect');
 
@@ -15,11 +17,30 @@ const handlebars = require('handlebars');
 const gulpHandlebars = require('gulp-handlebars-html')(handlebars);
 const rename = require('gulp-rename');
 
-handlebars.registerHelper('firstkeyval', function(obj) {
-  for (var i in obj){
-      return obj[i];
-      break;
-  }
+const markdown = require('gulp-marked-json');
+const HBS = require('gulp-hbs');
+
+/**
+ * I'm using two different handlebars plugins here and I hate it.
+ */
+[handlebars,HBS].forEach(function(h){
+  h.registerHelper('firstkeyval', function(obj) {
+    for (var i in obj){
+        return obj[i];
+        break;
+    }
+  });
+  
+})
+
+HBS.handlebars.logger.level = 0;
+
+fs.readdir('./src/templates/partials/', function(err, filenames) {
+    filenames.forEach(function(filename) {
+      fs.readFile('./src/templates/partials/' + filename, 'utf-8', function(err, content) {
+        HBS.registerPartial(filename.replace('.handlebars',''), content)
+      });
+    });
 });
 
 gulp.task('webserver', function() {
@@ -64,10 +85,21 @@ gulp.task('index', function () {
         .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('pages', function(){
+  gulp.src('./src/pages/*.md')
+    .pipe(markdown({
+        pedantic: true,
+        smartypants: true
+    }))
+    .pipe(HBS('./src/templates/page.handlebars'))
+    .pipe(gulp.dest('./dist'));
+})
+
+
 gulp.task('watch', function () {
-  gulp.watch('./src/**', ['sass', 'scripts', 'image','index']);
+  gulp.watch('./src/**', ['sass', 'scripts', 'image','index', 'pages']);
 });
 
-gulp.task('build', ['sass', 'scripts', 'image','index']);
+gulp.task('build', ['sass', 'scripts', 'image','index', 'pages']);
 
 gulp.task('default', ['build', 'watch', 'webserver']);
---
+++ src/pages/new-subscribers-giveaway.md
@@ -0,0 +1,18 @@
+---
+title: New Subscribers Giveaway
+description: New Subscribers can win a pack of fantastic comics from previous seasons!
+url: new-subscribers-giveaway.html
+share: ns-share.jpg
+---
+
+Are you a **new subscriber** this year? That's amazing - thanks! That's very much a cool and exciting thing to be.
+
+We thought me might give you a chance to catch up on some of the awesome comics we have sent out so far, something like this!  
+
+![Something like these](./img/new-subs.jpg)
+
+Just let us know, through [this form here](https://goo.gl/forms/xt4WhG4NbhW7vKX22), what you love about minicomics. We'll pick one and send you out a pack of sweet minicomics. Something like above - maybe even a few more if we can dig them up, you like suprises, right?
+
+Entries for this will close **31st January 2017**. Really it's only open to new subscribers - the prize is comics you already have otherwise.
+
+&hearts; Thanks we love you!
